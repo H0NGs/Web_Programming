2f4fdf125022ce890a4923d59de15c73b0e6e2fc
H0NGs <rhdfyddune99@gmail.com>
2023-11-23 14:30:15 +0900

    commit

--- board/board.php
+++ board/board.php
@@ -1,11 +1,61 @@
 <html>
     <head>
-        <meta charset="utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <link href="style.css" rel="stylesheet" />
+        <style type="text/css">
+            .           { font-family: Arial; font-size: 12px; color: black; line-height: 140%}
+            a           { font-family: Arial; font-size: 12px; color: #006699; text-decoration: none}
+            a:active    { font-family: Arial; font-size: 12px; color: #FF3333; text-decoration: none}
+            A:hover     { text-decoration; color:#ff9999}
+        </style>
     </head>
     <body>
-        <?php
-        
-        ?>
+        <table width="400" border=0 cellspacing="0" cellpadding="0">
+            <tr>
+                <td width=100% align=center><B>게 시 판</B></td>
+            </tr>
+            <tr>
+                <td width=100%>
+                    <?php
+                        //function Sub_List($num, $depth, $now_num, $table)
+                        $con = mysqli_connect("localhost", "root", "", "webpl");
+                        $pg_view_cnt = 10;
+                        $sql = "select num, root, reply, title, writer, wdate, count from board where reply=0 order by root desc";
+                        $list_result = mysqli_query($con, $sql);
+
+                        $totalCount = mysqli_num_rows($list_result);
+
+                        if ($totalCount > (floor($totalCount/$pg_view_cnt)) * $pg_view_cnt)
+                            $totalCount = floor($totalCount/$pg_view_cnt) + 1;
+                        else
+                            $totalCount = $totalCount / $pg_view_cnt;
+
+                        if ($page > 0)
+                            $page = $page;
+                        else
+                            $page = 1;
+                    ?>
+                    <table border=0 cellpadding=0 cellspacing=0 width=400>
+                        <tr align=center bgcolor=f0f8ff>
+                            <td width=30>번호</td>
+                            <td width=170>제목</td>
+                            <td width=50>글쓴이</td>
+                            <td width=100>날짜</td>
+                            <td width=50>조회수</td>
+                        </tr>
+                        <?php
+                            $i = 0; $j = 0;
+                            while ($list = mysqli_fetch_array($list_result))
+                            {
+                                $i = $i + 1;
+                                if (($page - 1) * $pg_view_cnt < $i) 
+                                {
+                                    $j = $j + 1;
+                                }
+                            }
+                        ?>
+                    </table>
+                </td>
+        </table>
     </body>
 </html>
