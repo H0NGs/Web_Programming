161c4510ba30ae51be3147a4f66a4b93ff3318fd
H0NGs <rhdfyddune99@gmail.com>
2023-11-23 17:39:34 +0900

    commit

--- board/board.php
+++ board/board.php
@@ -11,27 +11,30 @@
       <tr><td width=100%>
 
  <?php    // Main 시작 ===========================================
+
    include ("./include.inc");
 
    if (! isset($_GET['page'])) $page = 1;
    else  $page = $_GET['page'];
-
+   
    $pg_view_cnt=10;   // 한 페이지에 보여줄 게시물 개수
    
    $query="select num, root, reply, title, writer, wdate, count from board where reply=0 order by root desc";
    $list_result=mysqli_query($conn, $query);
    $totalCount=mysqli_num_rows($list_result);  // 게시물의 총 개수 구하기
-
+   
    if ($totalCount>(floor($totalCount/$pg_view_cnt))*$pg_view_cnt)    // 얻어온 게시물로 전체 페이지 수를 계산
        $totalCount=floor($totalCount/$pg_view_cnt)+1;
    else
        $totalCount=$totalCount/$pg_view_cnt;
+   
  ?>  
      <table border=0 cellpadding=0 cellspacing=0 width=500>
          <tr  height=40 align=center  bgcolor=f0f8ff> <td width=50>번호</td> <td width=200>제목</td> 
             <td width=100>글쓴이</td><td width=100>날짜</td><td width=50>조회수</td> </tr> 
 
 <?php 
+   
    $i=0;   // 읽어온 모든 게시물에 대해 반복 처리
    $j=0;   // 한 페이지에 보여줄 최대 게시물 수에 도달했는지를 판단하기 위한 반복 변수
    while($list=mysqli_fetch_array($list_result))  {  // 게시물 목록 출력 부분
@@ -45,29 +48,29 @@
 	      print "<td align=center>".$list["count"]."</td></tr>";
          print "<tr><td colspan=5>---------------------------------------------------------------------------------------</td></tr>";
          
-         //Sub_List($list["num"], 1, $list["num"], $page, $conn);  // 현재 출력한 게시물에 대한 댓글이 있으면 출력하기 위해 함수 호출
+         // Sub_List($list["num"], 1, $list["num"], $page, $conn);  // 현재 출력한 게시물에 대한 댓글이 있으면 출력하기 위해 함수 호출
       }
       if ($j==$pg_view_cnt) break;
    }
 
-   // if ($j<$pg_view_cnt && $j!=0) {
-	// 	for ($k=$j; $k<$pg_view_cnt; $k++)	{
-   //       if ($k+1<$pg_view_cnt)	{
-   //          print "<tr height=20><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
-   //          print "<tr><td colspan=5>---------------------------------------------------------------------------------------</td></tr>";
-   //       }
-   //       else {
-   //          print "<tr height=20><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
-   //          print "<tr><td colspan=5>===================================================</td></tr>";				
-   //       }
-   //    }
-   // }
+   if ($j<$pg_view_cnt && $j!=0) {
+		for ($k=$j; $k<$pg_view_cnt; $k++)	{
+         if ($k+1<$pg_view_cnt)	{
+            print "<tr height=20><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
+            print "<tr><td colspan=5>---------------------------------------------------------------------------------------</td></tr>";
+         }
+         else {
+            print "<tr height=20><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
+            print "<tr><td colspan=5>===================================================</td></tr>";				
+         }
+      }
+   }
 
    if ($j==0) {
       print "<tr height=300><td colspan=5 align=center>.......... 등록된 내용이 없습니다 ..........</td></tr>";
       print "<tr><td colspan=5 align=center>===================================================</td></tr>";
    }
-
+   
  ?>
       </table> <br>
 
