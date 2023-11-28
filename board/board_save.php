b3795e986bcc4d07720a14d57d5c785cc523e33c
H0NGs <rhdfyddune99@gmail.com>
2023-11-28 15:47:23 +0900

    commit

--- board/board_save.php
+++ board/board_save.php
@@ -36,6 +36,9 @@
                 //$file_path = "files\\".$filename; // Windows 전용
                 $file_path = "files/".$filename; // Unix 기반 전용
             }
+            else {
+                echo "alert('첨부파일을 업로드하는데 실패하였습니다!'); ";
+            }
 
             date_default_timezone_set('Asia/Seoul');
             $wdate = date("H:i:s");
