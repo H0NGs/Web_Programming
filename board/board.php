49d4ac48416d8cb949b2686c9b417fc338713ce4
H0NGs <rhdfyddune99@gmail.com>
2023-12-06 22:07:56 +0900

    commit

--- board/board.php
+++ board/board.php
@@ -1,12 +1,12 @@
-<html style="display:flex;justify-content:center;align-items:center;">
+<html style="display:flex;justify-content:center;align-items:center;font-size:20px;">
 <head>
       <title>board</title>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
       <link href="style.css" rel="stylesheet">
 </head>
 <body>
-   <table width="500" border=0 cellspacing="0" cellpadding="0">
-      <tr><td width=100% height=20 align=center><B>게 시 판</B></td></tr>	    
+   <table width="1000" border=0 cellspacing="0" cellpadding="0">
+      <tr><td width=100% height=40 align=center><B>게 시 판</B></td></tr>	    
       <tr><td width=100%>
 
  <?php    // Main 시작 ===========================================
@@ -27,9 +27,9 @@
        $totalCount=$totalCount/$pg_view_cnt;
    
  ?>  
-     <table border=0 cellpadding=0 cellspacing=0 width=500>
-         <tr  height=40 align=center  bgcolor=f0f8ff> <td width=50>번호</td> <td width=200>제목</td> 
-            <td width=100>글쓴이</td><td width=100>날짜</td><td width=50>조회수</td> </tr> 
+     <table border=0 cellpadding=0 cellspacing=0 width=1000>
+         <tr height=40 align=center bgcolor=f0f8ff> <td width=100>번호</td> <td width=400>제목</td> 
+            <td width=200>글쓴이</td><td width=200>날짜</td><td width=100>조회수</td> </tr> 
 
 <?php 
 
@@ -39,12 +39,12 @@
       $i=$i+1;
 	   if (($page-1)*$pg_view_cnt < $i)  {  // 출력할 해당 페이지를 찾아 레코드 출력
          $j=$j+1;
-	      print "<tr height=15 valign=bottom>   <td align=center>".$list["root"]."</td>";
+	      print "<tr height=30 valign=bottom>   <td align=center>".$list["root"]."</td>";
 	      print "<td><a href=board_view.php?page=".$page."&num=".$list["num"] . ">" . $list["title"]."</a></td>";
 	      print "<td align=center>".$list["writer"]."</td>";      
          print "<td align=center>".$list["wdate"]."</td>";
 	      print "<td align=center>".$list["count"]."</td></tr>";
-         print "<tr><td colspan=5>---------------------------------------------------------------------------------------</td></tr>";
+         print "<tr><td colspan=5>----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</td></tr>";
          
          Sub_List($list["num"], 1, $list["num"], $page, $conn);  // 현재 출력한 게시물에 대한 댓글이 있으면 출력하기 위해 함수 호출
       }
@@ -54,29 +54,29 @@
    if ($j<$pg_view_cnt && $j!=0) {
 		for ($k=$j; $k<$pg_view_cnt; $k++)	{
          if ($k+1<$pg_view_cnt)	{
-            print "<tr height=20><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
-            print "<tr><td colspan=5>---------------------------------------------------------------------------------------</td></tr>";
+            print "<tr height=80><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
+            print "<tr><td colspan=5>----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</td></tr>";
          }
          else {
-            print "<tr height=20><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
-            print "<tr><td colspan=5>===================================================</td></tr>";				
+            print "<tr height=40><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
+            print "<tr><td colspan=5>==========================================================================================================================</td></tr>";				
          }
       }
    }
 
    if ($j==0) {
-      print "<tr height=300><td colspan=5 align=center>.......... 등록된 내용이 없습니다 ..........</td></tr>";
-      print "<tr><td colspan=5 align=center>===================================================</td></tr>";
+      print "<tr height=600><td colspan=5 align=center>.......... 등록된 내용이 없습니다 ..........</td></tr>";
+      print "<tr><td colspan=5 align=center>==========================================================================================================================</td></tr>";
    }
    
  ?>
       </table> <br>
 
-      <table border=0 cellpadding=0 cellspacing=0 width=400>
-      <tr valign=center><td width=80>
+      <table border=0 cellpadding=0 cellspacing=0 width=800>
+      <tr valign=center><td width=160>
        <?php  if ($page>$pg_view_cnt)  print "<a href=board.php?page=".(floor(($page-1)/$pg_view_cnt)*$pg_view_cnt).">◀</a>";  ?>
          </td>
-         <td width=300>
+         <td width=600>
        <?php   // 페이지 번호 목록 출력 부분
          if ($i==($page*$pg_view_cnt) || $page>1) {
             for ($i=(floor(($page-1)/$pg_view_cnt)*$pg_view_cnt+1); $i<=((floor(($page-1)/$pg_view_cnt))+1)*$pg_view_cnt; $i++) {
@@ -91,7 +91,7 @@
              else if($i>0) print "<B>1 </B> ";
       ?>
           </td>	
-          <td width=80>
+          <td width=160>
             <?php  // 다음 페이지목록 링크
                $i=$i-1;
                if ($i<$totalCount && $i>0)  print "<a href=board.php?page=".($i+1).">▶</a>";
